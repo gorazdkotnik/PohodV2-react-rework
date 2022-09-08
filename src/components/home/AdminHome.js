262533dbcab2426016cc7ed6564e8e9e05f03e7f
AnzeBlaBla <anzemajblabla@gmail.com>
2022-09-08 12:18:34 +0200

    Dodan primer generacije qr kode

--- src/components/home/AdminHome.js
+++ src/components/home/AdminHome.js
@@ -6,6 +6,10 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 
+/* Primer generacije qr kode */
+import QRCode from "react-qr-code";
+import Box from '@mui/material/Box';
+
 const AdminHome = () => {
   return (
     <Card>
@@ -23,6 +27,15 @@ const AdminHome = () => {
           <Typography variant="h4">Pohod V2 - Admin</Typography>
           <Typography variant="h6">
             Aplikacija za organizacijo in izvedbo orientacijskega pohoda.
+            <Box sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              flexDirection: 'row',
+              my: 5
+            }}>
+              <QRCode value="hey" />
+            </Box>
           </Typography>
         </Stack>
       </CardContent>
