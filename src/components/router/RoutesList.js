e151065ce50fb4103ca5e1579bf90c2d25578896
gorazdkotnik <gorazdkotnik05@gmail.com>
2022-09-16 11:02:17 +0200

    added lazy loading for RouterList component

--- src/components/router/RoutesList.js
+++ src/components/router/RoutesList.js
@@ -11,17 +11,19 @@ import useUser from '../../hooks/useUser';
 
 import Home from '../../pages/Home';
 import Login from '../../pages/Login';
-import Profile from '../../pages/Profile';
-import Results from '../../pages/Results';
-import Leaderboard from '../../pages/Leaderboard';
-import Dashboard from '../../pages/dashboard/Dashboard';
-import DashboardDetails from '../../pages/dashboard/DashboardDetails';
-import Events from '../../pages/events/Events';
 import QuestionGroups from '../../pages/question_groups/QuestionGroups';
-import Groups from '../../pages/groups/Groups';
-import PointQuestions from '../../pages/PointQuestions';
 
-import NoPage from '../../pages/NoPage';
+const Leaderboard = React.lazy(() => import('../../pages/Leaderboard'));
+const Events = React.lazy(() => import('../../pages/events/Events'));
+const Groups = React.lazy(() => import('../../pages/groups/Groups'));
+const NoPage = React.lazy(() => import('../../pages/NoPage'));
+const Profile = React.lazy(() => import('../../pages/Profile'));
+const Results = React.lazy(() => import('../../pages/Results'));
+const PointQuestions = React.lazy(() => import('../../pages/PointQuestions'));
+const Dashboard = React.lazy(() => import('../../pages/dashboard/Dashboard'));
+const DashboardDetails = React.lazy(() =>
+  import('../../pages/dashboard/DashboardDetails')
+);
 
 function RoutesList() {
   const { user } = useUser();
@@ -30,65 +32,74 @@ function RoutesList() {
 
   return (
     <>
-      {/* Navbar */}
-      <Navbar user={user} />
-      <Container sx={{ mt: 15, mb: 5 }}>
-        <Routes>
-          {/* Home Page */}
-          {<Route exact path="/" element={<Home user={user} />} user={user} />}
+      <React.Suspense fallback={<div>Loading...</div>}>
+        {/* Navbar */}
+        <Navbar user={user} />
+        <Container sx={{ mt: 15, mb: 5 }}>
+          <Routes>
+            {/* Home Page */}
+            {
+              <Route
+                exact
+                path="/"
+                element={<Home user={user} />}
+                user={user}
+              />
+            }
 
-          {/* Login Page */}
-          <Route exact path="/login" element={<Login />} />
+            {/* Login Page */}
+            <Route exact path="/login" element={<Login />} />
 
-          {/* Profile Page */}
-          {userExists() && (
-            <Route exact path="/profile" element={<Profile user={user} />} />
-          )}
+            {/* Profile Page */}
+            {userExists() && (
+              <Route exact path="/profile" element={<Profile user={user} />} />
+            )}
 
-          {/* Leaderboard Page */}
-          {userExists() && user.group && (
-            <Route exact path="/results" element={<Results />} />
-          )}
+            {/* Leaderboard Page */}
+            {userExists() && user.group && (
+              <Route exact path="/results" element={<Results />} />
+            )}
 
-          {/* Leaderboard Page */}
-          {userExists() && (
-            <Route exact path="/leaderboard" element={<Leaderboard />} />
-          )}
+            {/* Leaderboard Page */}
+            {userExists() && (
+              <Route exact path="/leaderboard" element={<Leaderboard />} />
+            )}
 
-          {/* Dashboard */}
-          {userExists() && user.user_type === userTypes.ADMIN && (
-            <Route path="/dashboard" element={<Dashboard />} />
-          )}
+            {/* Dashboard */}
+            {userExists() && user.user_type === userTypes.ADMIN && (
+              <Route path="/dashboard" element={<Dashboard />} />
+            )}
 
-          {/* Dashboard Details */}
-          {userExists() && user.user_type === userTypes.ADMIN && (
-            <Route path="/dashboard/:id" element={<DashboardDetails />} />
-          )}
+            {/* Dashboard Details */}
+            {userExists() && user.user_type === userTypes.ADMIN && (
+              <Route path="/dashboard/:id" element={<DashboardDetails />} />
+            )}
 
-          {/* Events */}
-          {userExists() && user.user_type === userTypes.ADMIN && (
-            <Route path="/events/*" element={<Events />} />
-          )}
+            {/* Events */}
+            {userExists() && user.user_type === userTypes.ADMIN && (
+              <Route path="/events/*" element={<Events />} />
+            )}
 
-          {/* Question groups */}
-          {userExists() && user.user_type === userTypes.ADMIN && (
-            <Route path="/question_groups/*" element={<QuestionGroups />} />
-          )}
+            {/* Question groups */}
+            {userExists() && user.user_type === userTypes.ADMIN && (
+              <Route path="/question_groups/*" element={<QuestionGroups />} />
+            )}
 
-          {/* Groups */}
-          {userExists() && user.user_type === userTypes.USER && (
-            <Route path="/groups/*" element={<Groups user={user} />} />
-          )}
+            {/* Groups */}
+            {userExists() && user.user_type === userTypes.USER && (
+              <Route path="/groups/*" element={<Groups user={user} />} />
+            )}
 
-          {/* Point Questions */}
-          {userExists() && user.user_type === userTypes.USER && (
-            <Route path="/points/:hash" element={<PointQuestions />} />
-          )}
+            {/* Point Questions */}
+            {userExists() && user.user_type === userTypes.USER && (
+              <Route path="/points/:hash" element={<PointQuestions />} />
+            )}
 
-          {/* 404 Page */}
-          <Route path="*" element={<NoPage />} />
-        </Routes>
-      </Container>
+            {/* 404 Page */}
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+        </Container>
+      </React.Suspense>
     </>
   );
 }
