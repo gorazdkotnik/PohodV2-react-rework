0932c908d8fe4d12e2cec26aea396dbe8235a2cf
Gorazd Kotnik <59970689+gorazdkotnik@users.noreply.github.com>
2022-09-18 17:29:23 +0200

    login button style improve

--- src/components/auth/LoginButton.js
+++ src/components/auth/LoginButton.js
@@ -1,15 +1,11 @@
-
 import Button from '@mui/material/Button';
-const LoginButton = ({url}) => {
-    return (
-        <Button
-            variant="contained"
-            component="a"
-            href={url}
-        >
-            <img src='/images/login.png' alt='login' />
-        </Button>
-    );
+
+const LoginButton = ({ url }) => {
+  return (
+    <Button variant="contained" component="a" href={url} sx={{ p: 0 }}>
+      <img src="/images/login.png" alt="login" />
+    </Button>
+  );
 };
 
 export default LoginButton;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
 import useProtectedRoute from '../hooks/useProtectedRoute';
-import LoginForm from '../components/auth/LoginForm';
+// import LoginForm from '../components/auth/LoginForm';
 import LoginButton from '../components/auth/LoginButton';
 import { BACKEND_URL, FRONTEND_URL } from './../config/env';
 
@@ -13,15 +17,31 @@ const Login = () => {
   return (
     <Container maxWidth="sm">
       {/* <LoginForm /> */}
-      <LoginButton url={`https://login.microsoftonline.com/f6232921-d0d7-4c1a-9eee-0da15213004d/oauth2/v2.0/authorize?
+
+      <Card>
+        <CardContent>
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <LoginButton
+              url={`https://login.microsoftonline.com/f6232921-d0d7-4c1a-9eee-0da15213004d/oauth2/v2.0/authorize?
 client_id=dbfa2de4-b07a-4dd4-8e6b-509ce7766079
 &response_type=code
 &redirect_uri=${encodeURI(BACKEND_URL + '/auth/login')}
 &response_mode=query
 &scope=user.read
-&state=${btoa(JSON.stringify({
-  redirect: FRONTEND_URL
-}))}`} />
+&state=${btoa(
+                JSON.stringify({
+                  redirect: FRONTEND_URL,
+                })
+              )}`}
+            />
+          </Stack>
+        </CardContent>
+      </Card>
     </Container>
   );
 };
